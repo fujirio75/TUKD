942d85b2740cddad5732928054d509492ab2d9f9
フジモトリオ <fujirio75@fujirio75-MacbookPro13.local>
2018-10-19 01:42:04 +0900

    修正

--- archive.php
+++ archive.php
@@ -1,51 +1,38 @@
+<?php/*
+Template Name: Archive
+*/
+?>
+<?php get_header();?>
+
+	<?php
+$paged = (int) get_query_var('paged');
+$args = array(
+	'posts_per_page' => 6,
+	'paged' => $paged,
+	'orderby' => 'post_date',
+	'order' => 'DESC',
+	'post_type' => 'post',
+	'post_status' => 'publish'
+);
+$the_query = new WP_Query($args);
+if ( $the_query->have_posts() ) :
+	while ( $the_query->have_posts() ) : $the_query->the_post();
+?>
+	<div class="post">
+		<h1 class="title"><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+	</div>
+<?php endwhile; endif; ?>
+
 <?php
-/**
- * The template for displaying archive pages.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package design_gakubu
- */
+if ($the_query->max_num_pages > 1) {
+	echo paginate_links(array(
+		'base' => get_pagenum_link(1) . '%_%',
+		'format' => 'page/%#%/',
+		'current' => max(1, $paged),
+		'total' => $the_query->max_num_pages
+	));
+}
+?>
 
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php wp_reset_postdata(); ?>
